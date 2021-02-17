5b9838dd383dc069c4e6b7e48257bca052d5b72d
mcoope31 <mscoops@gmail.com>
2021-02-16 18:29:13 -0600

    Init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueTailwindColorPicker from '@/vue-tailwind-color-picker.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueTailwindColorPicker
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-tailwind-color-picker />
+  </div>
+</template>
